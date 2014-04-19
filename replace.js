622b7e0863643532a1cd5c2536e54ea10002b28d
coty <sutherland.coty@gmail.com>
2014-04-19 13:43:43 -0400

    Adding options page

--- replace.js
+++ replace.js
@@ -1,23 +1,19 @@
-$.getJSON("file://wordlist.json", function (data) {
-    console.debug("Loading wordlist.json");
-}).done(function () {
-    var items = [];
-    $.each( data, function( key, val ) {
-        //items.push(key + " " + val);
-        console.debug(key);
-    });
-   
-    // This is specific to SFDC case comments. Might want to make it more generic. Or go the other way and make it SFDC exclusive. 
-    $('input[id=newCaseCommentButton]').click(function () {
-        setTimeout(function () {
-            console.debug("Delaying call by .5 seconds.");
-            var areaSelector = $('textarea[id=new_text]');
-            areaSelector.keyup(replaceKeywords(areaSelector));
-        }, 500);
-    });
+// This is specific to SFDC case comments. Might want to make it more generic. Or go the other way and make it SFDC exclusive. 
+$('input[id=newCaseCommentButton]').click(function () {
+    setTimeout(function () {
+        console.debug("Delaying call by .5 seconds.");
+        var areaSelector = $('textarea[id=new_text]');
+        areaSelector.keyup(replaceKeywords(areaSelector));
+    }, 500);
 });
 
 function replaceKeywords(input) {
     console.debug("replaceKeywords called");
     input.val(input.val().replace(':keyword:', 'keyword sentence junk'));
+    chrome.storage.sync.get({
+        favoriteColor: 'red',
+        likesColor: true
+    }, function(items) {
+        console.info(items);
+    });
 }
