1699d901a7f408dc74b9b6568dd0d97eea719287
Coty Sutherland <sutherland.coty@gmail.com>
2014-04-27 19:46:40 -0400

    tweaking options still

--- options.js
+++ options.js
@@ -1,32 +1,77 @@
 // Saves options to chrome.storage
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
+    var options = {};
+
+    $("#mytable tr").each(function () {
+        // this is broken. it grabs all td's for each row. need to iterate over each individual td for each row.
+        $('td').each(function () {
+            var tarea = $(this).find('textarea');
+
+            if (tarea.length > 0) {
+                var key = tarea.attr('id');
+                var value = tarea.text();
+                console.debug('key: ' + key + ' value: ' + value);
+            }
+        })
+    });
+
+    // initial testing
+    chrome.storage.sync.set(options, function(items) {
+        for (key in items) {
+            console.debug("Saving: \'" + items[key] + "into " + key);
+        }
+        
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+          status.textContent = '';
+        }, 750);
+    });
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
+    console.debug("restore_options called");
+
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get(null, function(items) {
+        // document.getElementById('test').value = items.test;
+        for (key in items) {
+            console.debug("key: " + key + ", value: " + items[key]);
+            append_to_table(key, items[key]);
+        }
+    });
+}
+
+function append_to_table(option_name, option_value) {
+    option_value = typeof option_value !== 'undefined' ? option_value : '';
+
+    $('#mytable tr:last').after('<tr>')
+        .after('<td><textarea id=\'' + option_name + '\'>' + option_value + '</textarea></td>')
+        .after('<td><label>' + option_name + '</label></td>')
+        .after('</tr>');
+}
+
+function add_option() {
+    name = prompt("What is the keyword?");
+
+    if (name.length > 0 && name != 'null') {
+        chrome.storage.sync.get(name, function(items) {
+            var value = null;
+            for (key in items) {
+                value = key;
+            }
+           
+            // if value == null then the key did not exist previously 
+            if (value == null) {
+                append_to_table(name);
+            }
+        });
+    }
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('add').addEventListener('click', add_option);
--- replace.js
+++ replace.js
@@ -1,19 +1,51 @@
-// This is specific to SFDC case comments. Might want to make it more generic. Or go the other way and make it SFDC exclusive. 
-$('input[id=newCaseCommentButton]').click(function () {
-    setTimeout(function () {
-        console.debug("Delaying call by .5 seconds.");
-        var areaSelector = $('textarea[id=new_text]');
-        areaSelector.keyup(replaceKeywords(areaSelector));
-    }, 500);
-});
+(function () {
+    var keyword = "";
+    var buffer = false;
+    var replace = false;
 
-function replaceKeywords(input) {
-    console.debug("replaceKeywords called");
-    input.val(input.val().replace(':keyword:', 'keyword sentence junk'));
-    chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
-    }, function(items) {
-        console.info(items);
+    // This is specific to SFDC case comments. Might want to make it more generic. Or go the other way and make it SFDC exclusive. 
+    $('input[id=newCaseCommentButton]').click(function () {
+        setTimeout(function () {
+            console.debug("Delaying call by .5 seconds.");
+            var areaSelector = $('textarea[id=new_text]');
+            areaSelector.keyup(function (event) {
+                var key = String.fromCharCode(event.which);
+                var keyCode = event.which;
+                //console.debug(key);
+                //console.debug(keyCode);
+                if (keyCode == 186) {
+                    if (buffer) {
+                        buffer = false;
+                        replace = true;
+                    } else {
+                        buffer = true;
+                    }
+                    key = ""; // remove the prepended ':'
+                    console.debug("buffer == " + buffer);
+                }
+
+                if (buffer && key.length > 0) {
+                    keyword = keyword + key;
+                    console.debug("Appended " + key + " to " + keyword);
+                }
+
+                if (keyword != "" && replace) {
+                    console.debug("Replacing \'" + keyword + "\'");
+                    replaceKeywords(areaSelector, keyword.toLowerCase()); 
+                    keyword = "";
+                }
+            });
+        }, 500);
+    });
+})();
+
+function replaceKeywords(input, keyword) {
+    console.debug("replaceKeywords called with \'" + keyword + "\'");
+    chrome.storage.sync.get(keyword, function(items) {
+        for (key in items) {
+            console.debug("Key: " + key + ", Value: " + items[key]);
+            input.val(input.val().replace(":" + key + ":", items[key]));
+        }
     });
 }
+
